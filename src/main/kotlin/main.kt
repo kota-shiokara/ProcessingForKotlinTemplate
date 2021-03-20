b25818ed9d146849d039d3f3e20b8487b9df8cc3
kota-shiokara <ikanoshiokara.fun@gmail.com>
2021-03-21 02:12:14 +0900

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+import processing.core.*
+
+class Main : PApplet(){
+    override fun settings() {
+
+    }
+
+    override fun setup() {
+
+    }
+
+    override fun draw() {
+
+    }
+}
+
+fun main() = PApplet.main(Main::class.java.simpleName)
